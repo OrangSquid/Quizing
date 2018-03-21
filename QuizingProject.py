ce102556920f77bd3c9115ca542788046d960a8c
Rafael Matos <rafael.dom.matos@gmail.com>
2018-03-21 21:56:57 +0000

    Fix QuizingProject.py This commit also includes other minor things like: - When choosing either to make or exit from the script eith no file created, now the order of the operations is correct (was 1, 3 before) - choose_path now takes **kwargs - All modules except for QuzingProject are on a folder called Assets also meaning that the import statements in QuizingProject also changes as well as all their calls - Other fixes that I'm lazy to enumerate - THIS BITCH EMPTY YEEEEETTTT

--- QuizingProject.py
+++ QuizingProject.py
@@ -2,17 +2,128 @@ import os
 import sys
 import getpass
 import json
-from Assets import *
+import Assets.QuizingCore as QuizingCore
+import Assets.QuizingEditor as QuizingEditor
+import Assets.QuizingPlayer as QuizingPlayer
 from colorama import *
 
 # This is to make sure that when using colorama the color goes back into the original form
-colorama.init(autoreset = True)
+init(autoreset = True)
 
 print(Style.BRIGHT + "Welcome to the Quizing Project!\n")
 
-# TODO: fix this thing
+# This is just for you ;)
+class ThisBitchEmptyError(Exception):
+    """YEEEEEETTTT"""
+    pass
+
 def main():
-	pass
+
+    def choose_path(**kwargs):
+        """Diretcs the user to either QuizingEditor.py or QuzingPlayer.py"""
+        try:
+            with open(kwargs["file"], "r") as f:
+                temp = json.load(f)
+        except:
+            file_exists = False
+            print("Would you like to:\n")
+            print("1. Make a Quiz")
+            print(Fore.RED + Style.BRIGHT + "2. Exit")
+        else:
+            file_exists = True
+            print("Would you like to:\n")
+            print("1. Edit the Quiz")
+            print("2. Play the Quiz")
+            print(Fore.RED + Style.BRIGHT + "3. Exit\n")
+
+        while True:
+            action = input()
+
+            # Make or Edit Quiz
+            if action == "1":
+                if not file_exists:
+                    name = input("What will be the name of the file? ")
+                    with open("{}.quiz".format(name), "w") as f:
+                        json.dump(
+                            {"name" : name,
+                            "settings" : {},
+                            "questions" : []}, f)
+                    with open("{}.quiz".format(name), "r") as f:
+                        temp = json.load(f)
+                    quiz = QuizingCore.QuizCreateMode(temp["name"], 
+                                             temp["settings"], 
+                                             temp["questions"])
+                    quiz.set_settings()
+                else:
+                    quiz = QuizingCore.QuizCreateMode(temp["name"], 
+                                             temp["settings"], 
+                                             temp["questions"])
+                QuizingEditor.start_edit(quiz)
+
+            # Play Quiz
+            elif action == "2" and file_exists:
+                quiz = QuizingCore.QuizPlayMode(temp["name"], 
+                                       temp["settings"], 
+                                       temp["questions"])
+                QuizingPlayer.start_play(quiz)
+
+            # Exit with nonexistent file
+            elif action == "2" and not file_exists:
+                getpass.getpass("Press Enter to exit . . .")
+                sys.exit()
+
+            # Exit with existent file
+            elif action == "3":
+                getpass.getpass("Press Enter to exit . . .")
+                sys.exit()
+
+            else:
+                print("You must input a valid number!")
+
+    # try/except block to check for console arguments and if they're valid
+    # In case of failure 
+    try:
+        file = " ".join(sys.argv[1:])
+        if file == "":
+            raise ThisBitchEmptyError
+        elif os.path.isfile(file) and file.endswith(".quiz"):
+            print("File detected through console argument\n")
+            choose_path(file = file)
+        else:
+            print(Fore.BRIGHT + Fore.RED + "Invalid file passed!\n")
+            getpass.getpass("Press Enter to exit . . . ")
+            sys.exit()
+    except ThisBitchEmptyError:
+       filenames = os.listdir(os.curdir)
+
+       valid_options = []
+
+       # Checks if file has a valid extension
+       for filename in filenames:
+           if os.path.isfile(filename) and filename.endswith(".quiz"):
+               valid_options.append(filename)
+
+       if valid_options == []:
+           print("No file detected")
+           choose_path()
+       elif len(valid_options) == 1:
+           print("File detected in current directory: {}".format(valid_options[0]))
+           choose_path(file = valid_options[0])
+       else:
+           print("Multiple files detected in current directory")
+           print("Please choose one!\n")
+           option = 1
+           for file in valid_options:
+               print("{}. {}".format(option, file))
+               option += 1
+           while True:
+               try:
+                   option = int(input("\n"))
+               except:
+                   print("You must input a valid number!")
+                   continue
+               if option <= len(valid_options):
+                   choose_path(file = valid_options[option - 1])
 
 if __name__ == "__main__":
-	main()
+    main()
